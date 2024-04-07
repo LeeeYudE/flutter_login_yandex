478f1e7046a70e351a07d1df9fb27b27e24851fb
Mikhail Zharov <mike.zharov91@gmail.com>
2024-04-07 19:17:36 +0300

    Update FlutterLoginYandexPlugin.m

--- ios/Classes/FlutterLoginYandexPlugin.m
+++ ios/Classes/FlutterLoginYandexPlugin.m
@@ -1,11 +1,11 @@
 #import "FlutterLoginYandexPlugin.h"
-#if __has_include(<flutter_login_yandex/flutter_login_yandex-Swift.h>)
-#import <flutter_login_yandex/flutter_login_yandex-Swift.h>
+#if __has_include(<flutter_login_yandex_updated/flutter_login_yandex_updated-Swift.h>)
+#import <flutter_login_yandex_updated/flutter_login_yandex-Swift.h>
 #else
 // Support project import fallback if the generated compatibility header
 // is not copied when this plugin is created as a library.
 // https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
-#import "flutter_login_yandex-Swift.h"
+#import "flutter_login_yandex_updated-Swift.h"
 #endif
 
 @implementation FlutterLoginYandexPlugin
